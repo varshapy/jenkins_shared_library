2979fa83b4abb8e1edab02933faeb48e3958f856
varshapy <81504525+varshapy@users.noreply.github.com>
2021-04-23 14:55:25 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -47,8 +47,8 @@ pipeline {
 					stage('DEPLOY IMAGE') {
 						steps {
 						        sh "cd"
-							sh "ssh -i /rsa ubuntu@172.31.82.131"
-							sh "docker run alpine"           
+							sh "ssh -i /rsa ubuntu@172.31.82.131 deploy $registry $dockerTag $containerName"
+					                       
                 					}
 						
 					}
